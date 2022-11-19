29db578cad8bb90b54f9443bfd2436312bf4a861
Ravindra Kumar Saw <117341138+Ravindra9547@users.noreply.github.com>
2022-11-19 09:50:13 +0530

    Add files via upload

---
+++ 17-11-2022/LabAnimals.java
@@ -0,0 +1,33 @@
+package oopsconcept;
+class Animal
+{
+	public void eat()
+	{
+		System.out.println("Eat Method ");
+	}
+	public void sleep()
+	{
+		System.out.println("Sleep Method");
+	}
+}
+class Bird extends Animal
+{
+	
+	public void fly()
+	{
+		super.eat();//override the eat method
+		super.sleep();//override the sleep method
+		System.out.println("Fly Method ");
+	}
+}
+public class LabAnimals {
+public static void main(String []args)
+{
+	Animal ani=new Animal();
+	ani.eat();
+	ani.sleep();
+	System.out.println("------------");
+	Bird bir=new Bird();
+	bir.fly();
+}
+}
